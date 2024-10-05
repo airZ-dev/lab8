1a8b0686f4ff5dca348c40a356dcaf2ce3f6a8e5
Павленко Алексей <aleksej_pavlenko_05@mail.ru>
2024-10-05 06:14:54 +0300

    Changing time.pas again

--- time.pas
+++ time.pas
@@ -4,12 +4,12 @@ begin
   if year mod 4 = 0 then
   begin
     if (year mod 100 = 0) and (year mod 400 <> 0) then
-      println('Год не высокосный! 366')
+      println('Год не высокосный! 365')
     else
-      println('Год высокосный! 365')
+      println('Год высокосный! 366')
   end
   else 
-    println('Год не высокосный! 366');
+    println('Год не высокосный! 365');
 end.
 //1)Введите год >>  10
 //Год не высокосный!
